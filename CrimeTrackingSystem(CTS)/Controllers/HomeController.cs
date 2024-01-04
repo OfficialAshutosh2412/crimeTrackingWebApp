603c2282ce8693a529b2ad233024d8a1f3b6d3bc
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-01-04 21:24:24 +0530

    initial commit with ready designs of homepage, login page

---
+++ CrimeTrackingSystem(CTS)/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CrimeTrackingSystem_CTS_.Models;
+
+namespace CrimeTrackingSystem_CTS_.Controllers
+{
+    public class HomeController : Controller
+    {
+        CTSEntities _context = new CTSEntities();
+        //GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Contact
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        //POST: Contact
+        [HttpPost]
+        public ActionResult Contact(Contact contactFormData)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contact.Add(contactFormData);
+                _context.SaveChanges();
+                ModelState.Clear();
+                ViewBag.result = true;
+            }
+            return View();
+        }
+
+        //GET: Signup
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        //GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ CrimeTrackingSystem(CTS)/Scripts/custom.js
@@ -0,0 +1,50 @@
+﻿//navbar toggler
+document.querySelector('#login-tog').addEventListener("click", () => {
+    document.querySelector('.signin').classList.toggle('show-login');
+});
+//tools toggler
+document.querySelector('#tool-toggler').addEventListener("click", () => {
+    document.querySelector('.top-bar').classList.toggle('hide-top');
+});
+//preloader
+var load=document.getElementById("preloader");
+function myfunction() {
+    load.style.display="none"
+};
+//gallery revealer
+document.querySelector('#hide').addEventListener("click", () => {
+    document.querySelector('.ind-hide').classList.toggle('ind-show');
+});
+//closebtn
+document.querySelector('#close').addEventListener("click", () => {
+    document.querySelector('.ind-hide').classList.toggle('ind-show');
+});
+//chandrayan gallery revealer
+document.querySelector('#chandrahide').addEventListener("click", () => {
+    document.querySelector('.chandra-hide').classList.toggle('chandra-show');
+});
+//closebtn
+document.querySelector('#chandraclose').addEventListener("click", () => {
+    document.querySelector('.chandra-hide').classList.toggle('chandra-show');
+});
+//feedback
+function showFeed() {
+    document.querySelector('.feedback').classList.toggle('show-feeds');
+}
+//theme panel hidden
+function showTheme() {
+    document.querySelector('.themes').classList.toggle('theme-hide');
+    //auto hide theme panel
+   setTimeout(() => {
+       document.querySelector('.themes').classList.toggle('theme-hide');
+    }, 2500);
+}
+//show setting
+function showSetting() {
+    document.querySelector('.themeSwitch').classList.toggle('showtheme');
+    document.querySelector('.translator').classList.toggle('showtranslate');
+    document.querySelector('.feedbackBtn').classList.toggle('showfeed');
+    document.querySelector('.whats').classList.toggle('showwhats');
+}
+
+
