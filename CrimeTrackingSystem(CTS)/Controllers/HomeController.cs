ead3d53c639aef7485bfba48b597f66d04291923
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2024-01-06 18:25:44 +0530

    Backend of contact and Signup view

--- CrimeTrackingSystem(CTS)/Controllers/HomeController.cs
+++ CrimeTrackingSystem(CTS)/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,7 +10,7 @@ namespace CrimeTrackingSystem_CTS_.Controllers
 {
     public class HomeController : Controller
     {
-        CTSEntities _context = new CTSEntities();
+        readonly CTSEntities _context = new CTSEntities();
         //GET: Index
         public ActionResult Index()
         {
@@ -40,7 +41,63 @@ namespace CrimeTrackingSystem_CTS_.Controllers
         {
             return View();
         }
+        //POST: Signup
+        [HttpPost]
+        public ActionResult Signup(Signup formDataOfSignup)
+        {
+            //if form fields are validated
+            if (ModelState.IsValid == true)
+            {
+                //getting only name of file
+                string filenameWithoutExtension = Path.GetFileNameWithoutExtension(formDataOfSignup.ImageFile.FileName);
+                //getting file extension
+                string fileExtension = Path.GetExtension(formDataOfSignup.ImageFile.FileName);
+                //getting posted image file
+                HttpPostedFileBase postedFile = formDataOfSignup.ImageFile;
+                //getting length of posted file
+                int lengthOfFile = postedFile.ContentLength;
+                //now checking for filetype accepting only jpg, jpeg, JPEG and png files.
+                if (fileExtension.ToLower() == ".jpg" || fileExtension.ToLower() == ".jpeg" || fileExtension.ToLower() == ".JPEG" || fileExtension.ToLower() == ".png")
+                {
+                    //check if length is not more than 1MB
+                    if (lengthOfFile <= 1000000)
+                    {
+                        //setting actuall file with extension into filename without extension
+                        string actualFilename = filenameWithoutExtension + fileExtension;
+                        //passing path of image with folder into database
+                        formDataOfSignup.Photo = "~/UserProfilePicture/" + actualFilename;
+                        //mapping server image folder
+                        actualFilename = Path.Combine(Server.MapPath("~/UserProfilePicture/"), actualFilename);
+                        //saving the file into folder
+                        formDataOfSignup.ImageFile.SaveAs(actualFilename);
+                        if (formDataOfSignup.Role == "98u9d8uwr3(9ih(8H8&67^g&UyGIuh(7t6G^F4d4@A#24545fGbuyb*Y(8jIj(87Re54#qW")
+                        {
+                            formDataOfSignup.Role = "admin";    
+                            _context.Signup.Add(formDataOfSignup);
+                            _context.SaveChanges();
+                        }
+                        else if (formDataOfSignup.Role != "98u9d8uwr3(9ih(8H8&67^g&UyGIuh(7t6G^F4d4@A#24545fGbuyb*Y(8jIj(87Re54#qW")
+                        {
+                            formDataOfSignup.Role = "user";
+                            _context.Signup.Add(formDataOfSignup);
+                            _context.SaveChanges();
+                        }
 
+                        ModelState.Clear();
+                        return RedirectToAction("Login", "Home");
+                    }
+                    else
+                    {
+                        TempData["SizeError"] = "<script>alert('Error : file is larger than 1MB')</script>";
+                    }
+                }
+                else
+                {
+                    TempData["ExtensionError"] = "<script>alert('Error : file not supported')</script>";
+                }
+            }
+            return View();
+        }
         //GET: Login
         public ActionResult Login()
         {
--- CrimeTrackingSystem(CTS)/Models/Contact.cs
+++ CrimeTrackingSystem(CTS)/Models/Contact.cs
@@ -11,14 +11,29 @@ namespace CrimeTrackingSystem_CTS_.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
     public partial class Contact
     {
         public int Id { get; set; }
+
+        [Required]
         public string Fullname { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "invalid e-mail id")]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(\d{10}|\d{12})$", ErrorMessage = "invalid phone number")]
         public decimal Phone { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "not more than 50 words")]
         public string Purpose { get; set; }
+
+        [Required]
+        [StringLength(250,ErrorMessage ="not more than 250 words")]
         public string Details { get; set; }
     }
 }
